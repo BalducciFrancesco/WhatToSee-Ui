75498c81a53873d551fd532dae0603a04019c3c5
Francesco Balducci <balduccifrancesco382@gmail.com>
2023-01-31 15:07:05 +0100

    added tour search

--- src/app/pages/search-page/search-page.component.ts
+++ src/app/pages/search-page/search-page.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { City, TagDTO, Tour } from 'src/app/dtos/tour';
+import { CityService } from 'src/app/services/city.service';
+import { TourService } from 'src/app/services/tour.service';
 
 @Component({
   selector: 'app-search-page',
   templateUrl: './search-page.component.html',
   styleUrls: ['./search-page.component.scss']
 })
-export class SearchPageComponent {
+export class SearchPageComponent implements OnInit {
 
+  searchForm = new FormGroup({
+    cityId: new FormControl<number>(-1, { nonNullable: true }),
+    approxDuration: new FormControl<string>('', { nonNullable: true }),
+    tagsIds: new FormControl<number[]>([], { nonNullable: true }),
+    themeId: new FormControl<number>(-1, { nonNullable: true }),
+  })
+
+  cityOptions$!: Observable<City[]>
+  themeOptions$!: Observable<City[]>
+  tagsOptions$!: Observable<City[]>
+
+  searchResults$?: Observable<Tour[]>
+
+  constructor(
+    private cityService: CityService, 
+    private tourService: TourService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.cityOptions$ = this.cityService.getAll()
+    this.themeOptions$ = this.tourService.getAllThemes()
+    this.tagsOptions$ = this.tourService.getAllTags()
+  }
+
+  submitSearch(): void {
+    if(this.searchForm.valid) {
+      this.searchResults$ = this.tourService.search(this.searchForm.getRawValue())
+    }
+  }
+  
 }
--- src/app/services/tour.service.ts
+++ src/app/services/tour.service.ts
@@ -1,8 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { Tag, Theme, Tour, TourDTO } from '../dtos/tour';
+import { Tag, Theme, Tour, TourDTO, TourSearchDTO } from '../dtos/tour';
 
 @Injectable({
   providedIn: 'root'
@@ -17,6 +17,50 @@ export class TourService {
 
   public getAllThemes(): Observable<Theme[]> {
     return this.http.get<Theme[]>(environment.apiUrl + '/themes')
+  }
+
+  public search(s: TourSearchDTO): Observable<Tour[]> {
+    // return of([
+    //   {
+    //     id: 0,
+    //     author: {
+    //       id: 0,
+    //       username: 'oeoeo',
+    //       password: 'pass-segreta',
+    //       firstName: 'as',
+    //       lastName: 'ere',
+    //       organizationName: 'adskkda',
+    //       favouriteCity: {
+    //         id: 0,
+    //         name: 'torino'
+    //       }
+    //     },
+    //     title: 'Pepepe',
+    //     city: {
+    //       id: 0,
+    //       name: 'torino'
+    //     },
+    //     tags: [],
+    //     theme: {
+    //       id: 0,
+    //       name: 'eerere'
+    //     },
+    //     approxCost: 1,
+    //     approxDuration: '03:00',
+    //     creation: new Date(),
+    //     lastUpdate: new Date(),
+    //     stops: [],
+    //     reviews: [],
+    //     reports: [],
+    //     suggestions: []
+    //   }
+    // ])
+    const params = new HttpParams()
+      .append('cityId', s.cityId)
+      .append('duration', s.approxDuration)
+      .append('themeId', s.themeId)
+      .appendAll({'tagsIds': s.tagsIds})
+    return this.http.get<Tour[]>(environment.apiUrl + '/themes', { params })
   }
 
   public createTour(t: TourDTO): Observable<Tour> {
