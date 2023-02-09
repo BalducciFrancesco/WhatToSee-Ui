20f39d808bba88c4b995b933049b81a6dec67a5c
Francesco Balducci <francesco.balducci@edu.elis.org>
2023-02-09 18:46:07 +0100

    modified tag management

--- src/app/pages/tour-editor-page/tour-editor-page.component.ts
+++ src/app/pages/tour-editor-page/tour-editor-page.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { Observable } from 'rxjs';
 import { TourStopEditorDialogComponent } from 'src/app/components/tour-stop-editor-dialog/tour-stop-editor-dialog.component';
-import { City, Tag, TagDTO, Theme, TourStopDTO } from 'src/app/dtos/tour';
+import { City, Tag, TagDTO, Theme, Tour, TourStopDTO } from 'src/app/dtos/tour';
 import { CityService } from 'src/app/services/city.service';
 import { TourService } from 'src/app/services/tour.service';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { MatChipInputEvent } from '@angular/material/chips';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 
 // TODO reuse for editing / creation
+
+type TagControl = FormGroup<{
+  name: FormControl<string>
+}>
 
 @Component({
   selector: 'app-tour-editor-page',
@@ -20,8 +27,10 @@ export class TourEditorPageComponent implements OnInit {
   form = new FormGroup({
     title: new FormControl<string>('', { nonNullable: true, validators: Validators.required }),
     cityId: new FormControl<number>(-1, { nonNullable: true, validators: Validators.required }),
-    tagsIds: new FormControl<number[]>([], { nonNullable: true, validators: Validators.required }),
-    themeId: new FormControl<number>(-1, { nonNullable: true, validators: Validators.required }),
+    tags: new FormArray<TagControl>([], { validators: Validators.required }),
+    theme: new FormGroup({
+      name: new FormControl<string>('', { nonNullable: true, validators: Validators.required }),
+    }),
     approxCost: new FormControl<number>(-1, { nonNullable: true, validators: Validators.required }),
     approxDuration: new FormControl<string>('', { nonNullable: true, validators: Validators.required }),
     stops: new FormControl<TourStopDTO[]>([], { nonNullable: true, validators: Validators.required })
@@ -30,6 +39,8 @@ export class TourEditorPageComponent implements OnInit {
   cityOptions$!: Observable<City[]>
   tagsOptions$!: Observable<Tag[]>
   themeOptions$!: Observable<Theme[]>
+
+  separatorKeysCodes: number[] = [ENTER, COMMA];
 
   constructor(
     private cityService: CityService, 
@@ -46,13 +57,38 @@ export class TourEditorPageComponent implements OnInit {
   openDialog(): void {
     const dialogRef = this.dialogService.open(TourStopEditorDialogComponent, { data: this.form.controls.stops.value.length });
     dialogRef.afterClosed().subscribe(createdStop => {
-      this.form.patchValue({ stops: [...this.form.controls.stops.value, createdStop] })
+      if(createdStop)
+        this.form.patchValue({ stops: [...this.form.controls.stops.value, createdStop] })
     });
   }
 
   submit(): void {
-    if(this.form.valid) 
-      this.tourService.createTour(this.form.getRawValue())
+    if(this.form.valid) {
+      let t: any = this.form.getRawValue()
+      t.tags = 
+      this.tourService.createTour(this.form.getRawValue()).subscribe({
+        next: (t: Tour) => {
+          console.log('salvato');
+        }, error: (e: Error) => console.log(e)
+      })
+    }
+  }
+
+  addTag(e: MatChipInputEvent): void {
+    if (e.value.trim()) {
+      this.form.controls.tags.push(new FormGroup({ name: new FormControl<string>(e.value, { nonNullable: true }) }))
+      e.chipInput.clear()
+    }
+  }
+
+  removeTag(name: string): void {
+    const i = this.form.controls.tags.value.findIndex(t => t.name === name)
+    if(i !== -1)
+      this.form.controls.tags.removeAt(i)
+  }
+
+  selectedTag(e: MatAutocompleteSelectedEvent): void {
+    this.form.controls.tags.push(new FormGroup({ name: new FormControl<string>(e.option.viewValue, { nonNullable: true }) }))
   }
 
 }
